bc682d0cb5de1154d533323f01f6768a6c274366
Achanta Chittibabu <achantachittibabu@gmail.com>
2023-12-20 22:37:36 +0530

    Api gateway still having issue

--- Service2/src/main/java/com/spring/app2/Service2/Service2Application.java
+++ Service2/src/main/java/com/spring/app2/Service2/Service2Application.java
@@ -3,16 +3,18 @@ package com.spring.app2.Service2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/service2")
 public class Service2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service2Application.class, args);
 	}
-	@GetMapping
+	@GetMapping("/")
 	public String getName(){
 		return "Chittibabu";
 	}
--- Service3/src/main/java/com/spring/app3/Service3/Service3Application.java
+++ Service3/src/main/java/com/spring/app3/Service3/Service3Application.java
@@ -3,16 +3,18 @@ package com.spring.app3.Service3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/service3")
 public class Service3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service3Application.class, args);
 	}
-	@GetMapping
+	@GetMapping("/")
 	public String getClassName(){
 
 		return "5th Class";
