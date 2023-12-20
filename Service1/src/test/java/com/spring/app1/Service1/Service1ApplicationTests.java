1cd2d70c32206d6f8810824cb575429fecd3368b
Achanta Chittibabu <achantachittibabu@gmail.com>
2023-12-20 15:36:15 +0530

    service1 added

---
+++ Service1/src/test/java/com/spring/app1/Service1/Service1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.app1.Service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
