a2301150c129f469f8756903b348b526e3bf6fef
Achanta Chittibabu <achantachittibabu@gmail.com>
2023-12-20 17:25:10 +0530

    service3 added

--- Service2/src/main/java/com/spring/app2/Service2/Service2Application.java
+++ Service2/src/main/java/com/spring/app2/Service2/Service2Application.java
@@ -2,12 +2,19 @@ package com.spring.app2.Service2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Service2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service2Application.class, args);
+	}
+	@GetMapping
+	public String getName(){
+		return "Chittibabu";
 	}
 
 }
